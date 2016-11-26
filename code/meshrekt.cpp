12454ccb8e033f8c9eb451649f6c5bf819a2b037
rivten <rivten.grey@gmail.com>
2016-11-26 22:10:33 +0100

    clean save and read off functions

--- code/meshrekt.cpp
+++ code/meshrekt.cpp
@@ -1,36 +1,54 @@
-// TODO(hugo) : Choice of the kernel is important ! Think about it later or test with differents kernels how differents the results are
 #include <CGAL/Simple_cartesian.h>
-//#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/IO/Polyhedron_iostream.h>
 
 #include <fstream>
 #include <vector>
 
-/*
- * TODO(hugo)
- *      * Perform one basic mesh decimation
- *      * List all possible mesh contraction according to GH
- *      * Compute quadric errors
- *      * Perform GH algorithm
- *      * Measure error between input and output meshes
- */
-
+#define Assert(x) do{if(!(x)){*(int*)0=0;}}while(0)
 typedef CGAL::Simple_cartesian<double> K;
 typedef CGAL::Polyhedron_3<K> mesh;
 
 typedef K::Point_3 v3;
 
-struct plane_from_facet
+/*
+ * TODO(hugo)
+ *      * Perform one basic mesh decimation
+ *      * List all possible edge contraction according to GH
+ *      * Compute quadric errors for each edge contraction
+ *      * Solve minimization problem for a given edge contraction
+ *      * Perform GH algorithm
+ *      * Measure error between input and output meshes
+ *      * Compute one face planarity score
+ *      * Compute planar proxies
+ *      * Display planar proxies
+ *      * Compute quadric error according to SAMD
+ */
+
+mesh ReadOFF(char* Filename)
 {
-	mesh::Plane_3 operator()(mesh::Facet& f)
+	mesh Result;
+	std::ifstream File;
+	File.open(Filename);
+	if(!File.good())
 	{
-		mesh::Halfedge_handle h = f.halfedge();
-		return(mesh::Plane_3(h->vertex()->point(), 
- 							h->next()->vertex()->point(), 
-							h->opposite()->vertex()->point()));
+		std::cout << "File not found" << std::endl;
+		Assert(false);
 	}
-};
+	File >> Result;
+	File.close();
+
+	return(Result);
+}
+
+void SaveOFF(mesh* Mesh, const char* Filename)
+{
+	CGAL::set_ascii_mode(std::cout);
+	std::ofstream OFFFile;
+	OFFFile.open(Filename);
+	OFFFile << *Mesh;
+	OFFFile.close();
+}
 
 int main(int ArgumentCount, char** Arguments)
 {
@@ -39,24 +57,11 @@ int main(int ArgumentCount, char** Arguments)
 		std::cout << "You should give more arguments.\nTerminating." << std::endl;
 		return(1);
 	}
-	mesh InputMesh;
-	std::ifstream InputMeshFile;
-	InputMeshFile.open(Arguments[1]);
-	if(!InputMeshFile.good())
-	{
-		std::cout << "File not found" << std::endl;
-		return(1);
-	}
-	InputMeshFile >> InputMesh;
-	InputMeshFile.close();
+
+	mesh InputMesh = ReadOFF(Arguments[1]);
 	std::cout << "The input mesh contains " << InputMesh.size_of_vertices() << " vertices." << std::endl;
 
-	CGAL::set_ascii_mode(std::cout);
-	std::ofstream OFFFile;
-	OFFFile.open("convex_hull.off");
-	OFFFile << InputMesh;
-	OFFFile.close();
-	std::cout << "Done !" << std::endl;
+	SaveOFF(&InputMesh, "output.off");
 
 	return(0);
 }
